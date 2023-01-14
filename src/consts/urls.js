3406966fc5e8f7ed91b4e01ddfb4cfd165177c2c
aj2000 <hi@ajaysharma.dev>
2023-01-14 13:00:54 +0530

    updated backend url

--- src/consts/urls.js
+++ src/consts/urls.js
@@ -2,11 +2,11 @@
 // Local API server url
 // export const BASE_URL =  'http://127.0.0.1:8000'
 // Hosted API server url
-export const BASE_URL = "https://engage-autoapi.herokuapp.com";
+export const BASE_URL = "https://web-production-e3c8.up.railway.app";
 
-//default csv file link for sales forecast page 
+//default csv file link for sales forecast page
 export const demoCSVFileURL =
   "https://raw.githubusercontent.com/aj-2000/autoapi/main/App/datasets/demo_sales_dataset.csv";
 // 2nd file
-  // export const demoCSVFileURL = 
+// export const demoCSVFileURL =
 //   "https://raw.githubusercontent.com/aj-2000/autoapi/main/App/datasets/total_car_sales.csv"
